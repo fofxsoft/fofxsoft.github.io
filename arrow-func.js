484af85b3a6e98dc4afb66fc7205cdde4b47cb76
Michael Kellsy <mkellsy@gmail.com>
2018-09-20 11:35:51 -0600

    Update arrow-func.js

--- arrow-func.js
+++ arrow-func.js
@@ -1,25 +1,3 @@
-// Use named function expressions instead of function declarations.
-// ----------------------------------------------------------------------------------------
-// Function declarations are hoisted, which means that it’s easy - too easy - to reference
-// the function before it is defined in the file.
-
-// Always use const or let to declare variables.
-// ----------------------------------------------------------------------------------------
-// Not doing so will result in global variables. We want to avoid polluting the global
-// namespace.
-
-// Always use doublequotes for strings.
-// ----------------------------------------------------------------------------------------
-// This improves redability. Consistant string quotes helps with searching for strings in
-// a file.
-
-// When you use an anonymous function (as with callbacks), use arrow function notation.
-// ----------------------------------------------------------------------------------------
-// It creates a version of the function that executes in the context of this, which is
-// usually what you want, and is a more concise syntax.
-
-// BAD
-// ----------------------------------------------------------------------------------------
 function bad() {
     return new Promise(function(then) {
         setTimeout(function () {
@@ -34,8 +12,6 @@ bad().then(function(response) {
     alert(message);
 });
 
-// BETTER
-// ----------------------------------------------------------------------------------------
 const better = function () {
     return new Promise((then) => {
         setTimeout(() => {
@@ -50,8 +26,6 @@ better().then((response) => {
     alert(message);
 });
 
-// BEST
-// ----------------------------------------------------------------------------------------
 const best = function () {
     return new Promise(then => {
         setTimeout(() => then("Encompass Platform"), 300);
